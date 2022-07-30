51662fb990afb736396351b398d94ad2d08b11d4
kitatas <315kitatas10703@gmail.com>
2022-07-30 22:09:00 +0900

    review: バリデーション

--- Assets/Scripts/Game/Audio/AudioManager.cs
+++ Assets/Scripts/Game/Audio/AudioManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
@@ -40,7 +41,17 @@ namespace Game.Audio
 
         private AudioClip FindAudioClip(AudioType type)
         {
-            return audioDatas.Find(x => x.Type == type).Clip;
+            var data = audioDatas.Find(x => x.Type == type);
+            if (data == null)
+            {
+                throw new Exception($"audio data is null. (type: {type})");
+            }
+
+            if (data.Clip == null)
+            {
+                throw new Exception($"audio clip is null. (type: {type})");
+            }
+            return data.Clip;
         }
     }
 }
